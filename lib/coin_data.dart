ab054058130aec1dbefad6e6857284c2367cee75
AdityaKomawar <adityakomawar7@gmail.com>
2021-05-22 22:42:41 +0530

    App Completed

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,9 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+String apiKey = '5ECA2FD2-D603-4703-95B7-94460660797C';
+String url = 'https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=$apiKey';
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +34,16 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+class CoinData {
+  CoinData();
+  Future<double> getCoinData(String cryptoCurr, String currency) async {
+    url = 'https://rest.coinapi.io/v1/exchangerate/$cryptoCurr/$currency?apikey=$apiKey';
+    var response = await http.get(url);
+    // print(response.statusCode);
+    // print(response.body);
+    if (response.statusCode == 200)
+      return jsonDecode(response.body)['rate'];
+    else
+      return 0;
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'coin_data.dart';
+import 'main.dart';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,6 +8,56 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedValue = 'USD';
+  String selectedCryptoCurr = 'BTC';
+  var responseData;
+  double rate = 0;
+  String currency;
+  String cryptoCurrency;
+
+  CoinData coinData = CoinData();
+
+  List<DropdownMenuItem> getDropDownList() {
+    List<DropdownMenuItem<String>> dropDownItems = [];
+    for (currency in currenciesList) {
+      dropDownItems.add(
+        DropdownMenuItem(
+          child: Text(currency),
+          value: currency,
+        ),
+      );
+    }
+    return dropDownItems;
+  }
+
+  List<DropdownMenuItem> getDropDownCryptoList() {
+    List<DropdownMenuItem<String>> dropDownItems = [];
+    for (cryptoCurrency in cryptoList) {
+      dropDownItems.add(
+        DropdownMenuItem(
+          child: Text(cryptoCurrency),
+          value: cryptoCurrency,
+        ),
+      );
+    }
+    return dropDownItems;
+  }
+
+  void getData() async {
+    double tempRate =
+        await coinData.getCoinData(selectedCryptoCurr, selectedValue);
+    setState(() {
+      rate = tempRate;
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,7 +79,7 @@ class _PriceScreenState extends State<PriceScreen> {
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
-                  '1 BTC = ? USD',
+                  '1 $selectedCryptoCurr = ${rate.toStringAsFixed(2)} $selectedValue',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
@@ -42,7 +94,40 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                DropdownButton<String>(
+                  value: selectedCryptoCurr,
+                  items: getDropDownCryptoList(),
+                  onChanged: (value) async {
+                    CoinData coinData = CoinData();
+                    double tempRate =
+                        await coinData.getCoinData(value, selectedValue);
+                    setState(() {
+                      selectedCryptoCurr = value;
+                      rate = tempRate;
+                    });
+                  },
+                ),
+                SizedBox(
+                  width: 15.0,
+                ),
+                DropdownButton<String>(
+                  value: selectedValue,
+                  items: getDropDownList(),
+                  onChanged: (value) async {
+                    CoinData coinData = CoinData();
+                    double tempRate =
+                        await coinData.getCoinData(selectedCryptoCurr, value);
+                    setState(() {
+                      selectedValue = value;
+                      rate = tempRate;
+                    });
+                  },
+                ),
+              ],
+            ),
           ),
         ],
       ),
